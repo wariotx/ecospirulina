209fb0d293218a0a9f3d739abab80eb9094ff29b
wariotx <leopoldonegro@gmail.com>
2017-01-16 23:47:08 +0100

    setup sticky-nav

--- assets/js/sticky-navigation.js
+++ assets/js/sticky-navigation.js
@@ -1,1 +1,12 @@
-$(".storefront-primary-navigation").stick_in_parent();
+(function ($) {
+    $(".storefront-primary-navigation").children().wrapAll("<div class='sticky-nav'></div>");
+    var topSpacing = 0;
+    if ( $( 'body' ).hasClass( 'admin-bar' ) ) {
+        topSpacing = 32;
+    }
+
+    $(".sticky-nav").stick_in_parent({
+        bottoming: false,
+        offset_top: topSpacing
+    });
+})(jQuery);
