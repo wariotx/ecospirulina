9db62753b5cc8871e7d6c41e9611b1a9e3841715
wariotx <leopoldonegro@gmail.com>
2017-01-18 00:26:45 +0100

    Checked rememberme for woocommerce

--- functions.php
+++ functions.php
@@ -76,4 +76,13 @@ add_action('init', function (){
     remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
     remove_action('woocommerce_after_shop_loop', 'woocommerce_catalog_ordering', 10);
     remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 10);
+    add_action( 'woocommerce_login_form_end', function () {
+        echo <<<JS
+		<script type="text/javascript">
+			var checkbox = document.getElementById('rememberme');
+			if ( null != checkbox )
+				checkbox.checked = true;
+		</script>
+JS;
+    });
 });
