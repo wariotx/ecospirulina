36af1681585b20ac5d001a101f831c92ffc4d59c
wariotx <leopoldonegro@gmail.com>
2017-01-25 02:58:44 +0100

    Create empty template for landing

---
+++ template-empty.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *
+ * Template name: Vacío
+ *
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="theme-color" content="#ffffff">
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+    <div id="content" class="site-content" tabindex="-1">
+        <div class="col-full">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
+
+			<?php
+			/**
+			 * Functions hooked in to homepage action
+			 *
+			 * @hooked storefront_homepage_content      - 10
+			 * @hooked storefront_product_categories    - 20
+			 * @hooked storefront_recent_products       - 30
+			 * @hooked storefront_featured_products     - 40
+			 * @hooked storefront_popular_products      - 50
+			 * @hooked storefront_on_sale_products      - 60
+			 * @hooked storefront_best_selling_products - 70
+			 */
+//			do_action( 'homepage' );
+            storefront_homepage_content();
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<?php
+get_footer();
