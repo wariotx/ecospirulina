1780dc9cd0fae00196b247f5348e69c9563b2abb
wariotx <leopoldonegro@gmail.com>
2017-01-26 02:00:48 +0100

    Disable sticky-kit spacer

--- assets/js/sticky-navigation.js
+++ assets/js/sticky-navigation.js
@@ -1,12 +1,21 @@
-(function ($) {
-    $(".storefront-primary-navigation").children().wrapAll("<div class='sticky-nav'></div>");
-    var topSpacing = 0;
-    if ( $( 'body' ).hasClass( 'admin-bar' ) ) {
-        topSpacing = 32;
-    }
+jQuery(function () {
+    (function ($) {
+        $(".storefront-primary-navigation").children().wrapAll("<div class='sticky-nav'></div>");
+        var topSpacing = 0;
+        if ( $( 'body' ).hasClass( 'admin-bar' ) ) {
+            topSpacing = 32;
+        }
 
-    $(".sticky-nav").stick_in_parent({
-        bottoming: false,
-        offset_top: topSpacing
-    });
-})(jQuery);
+        $(".sticky-nav").stick_in_parent({
+            bottoming: false,
+            offset_top: topSpacing,
+            inner_scrolling: false,
+            spacer: false
+        }).on("sticky_kit:stick", function(e) {
+            console.log("has stuck!", e.target);
+        })
+            .on("sticky_kit:unstick", function(e) {
+                console.log("has unstuck!", e.target);
+            });
+    })(jQuery);
+});
