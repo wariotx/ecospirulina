3673158161513dc6dab7c0e501e6e6359432ee2c
wariotx <leopoldonegro@gmail.com>
2017-01-27 13:58:45 +0100

    use default header on template-empty

--- template-empty.php
+++ template-empty.php
@@ -4,26 +4,8 @@
  * Template name: Vacío
  *
  */
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/manifest.json">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="theme-color" content="#ffffff">
 
-    <?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-    <div id="content" class="site-content" tabindex="-1">
-        <div class="col-full">
+get_header(); ?>
         <div id="primary" class="content-area">
             <main id="main" class="site-main" role="main">
 
